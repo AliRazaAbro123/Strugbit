3ec65c6f63ff1995ebe5978f18657b5cf7f1da87
Ali Raza <156504784+AliRazaAbro123@users.noreply.github.com>
2025-02-06 01:24:28 -0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -35,7 +35,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="google-adsense-account" content="ca-pub-6153573963834456">
+        <meta name="google-adsense-account" content="ca-pub-6153573963834456" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <meta name="keywords" content={metadata.keywords} />
